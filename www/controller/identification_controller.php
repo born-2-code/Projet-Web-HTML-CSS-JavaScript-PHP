0d549eaa35207a228ad4faa54d2a5f04121eb203
Pro <pro@MacBook-pro-dErwan-Plessis.local>
2017-04-19 10:28:47 +0200

    add sign in

--- www/controller/identification_controller.php
+++ www/controller/identification_controller.php
@@ -4,7 +4,55 @@
 
 include 'singleton.php';
 
+if(isset($_SESSION['id'])){
+	header('Location: profile.html');
+}
+
+// =====================    SIGN IN     ========================= //
+
+if(isset($_POST['email']) && isset($_POST['pwd'])){
+
+	$email = htmlspecialchars($_POST['email']);
+	$pwd = htmlspecialchars($_POST['pwd']);
+
+	$check_user = $conn->prepare('SELECT Id_User FROM Users WHERE User_Password = :pwd AND Email = :email');
+	
+	$check_user->execute(array(
+		':pwd' => $pwd, 
+		':email' => $email
+	));
+
+	if($result = $check_user->fetch()){
+
+		$_SESSION['id'] = $result['Id_User'];
+		header('Location: profile.html');
+	}
+}
+
 // password_hash("password", PASSWORD_DEFAULT).=> Hash
 
+// =====================    SIGN UP     ========================= //
+
+if(isset($_POST['name']) && isset($_POST['surname']) && isset($_POST['email2']) && isset($_POST['password'] && isset($_POST['password_confirmation'])){
+
+	$name = htmlspecialchars($_POST['name']);
+	$surname = htmlspecialchars($_POST['surname']);
+	$email = htmlspecialchars($_POST['email2']);
+	$password = htmlspecialchars($_POST['password']);
+	$pwd = htmlspecialchars($_POST['password']);
+
+	$add_user = $conn->prepare('INSERT into Users (User_name, Surname, Email, User_password) VALUES ('$name','$surname', '$email2', '$password')');
+	
+	$add_user->execute(array(
+		':pwd' => $pwd, 
+		':email' => $email
+	));
+
+	if($result = $check_user->fetch()){
+
+		$_SESSION['id'] = $result['Id_User'];
+		header('Location: profile.html');
+	}
+}
 
 ?>
---
+++ www/index.php
@@ -0,0 +1,59 @@
+<?php
+include 'identification_controller.php';
+?>
+
+<!DOCTYPE html>
+<html >
+<head>
+  <meta charset="UTF-8">
+  <title>BDE eXia</title>
+  <link rel="stylesheet" href="css/login_style.css">
+	</head>
+		<body>
+  		<div class="login-page">
+  		<div class="form">
+    	<form class="register-form">
+    	<h2>SIGN UP</h2>
+      	<input type="text" name='name' placeholder="Name"/>
+      	<input type="text" name='surname' placeholder="Surname"/>
+			<div>
+			<label class="ecole">
+			</label>
+			<label class="radio-button radio-button--material">
+  			<input type="radio" class="radio-button__input radio-button--material__input" name="r" checked="checked">
+  			<div class="radio-button__checkmark radio-button--material__checkmark"></div>
+  			eXia  
+</label>
+<label class="radio-button radio-button--material">
+  <input type="radio" class="radio-button__input radio-button--material__input" name="r">
+  <div class="radio-button__checkmark radio-button--material__checkmark"></div>
+  EI
+ 
+<label class="radio-button radio-button--material">
+  <input type="radio" class="radio-button__input radio-button--material__input" name="r">
+  <div class="radio-button__checkmark radio-button--material__checkmark"></div>
+  CESI Alternance
+  <p></p>
+   </label>
+  </div>
+</label>
+      <input type="text" name='email2' placeholder="email"/>
+      <input type="password" name='password' placeholder="Password"/>
+      <input type="password" name='password_confirmation' placeholder="Confirm password"/>
+      <button>SIGN UP</button>
+      <p class="message">Alreary Registered ? <a href="#">Sign in</a></p>
+    </form>
+    <form method="POST" class="login-form">
+    <h2>SIGN IN</h2>
+      <input type="text" name='email' placeholder="email"/>
+      <input type="password" name='pwd' placeholder="Password"/>
+      <button type="submit">SIGN IN</button>
+      <p class="message">Don't have an account ? <a href="#">Sign up</a></p>
+      <p class="message">Forgot password ? <a href="reset.html#">Reset</a></p>
+    </form>
+  </div>
+</div>
+  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+    <script src="js/index.js"></script>
+</body>
+</html>
